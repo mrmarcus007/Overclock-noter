0f4bece95a040bc07600748d36a7f5bb950868eb
mrmarcus007 <mrmarcus00700909@gmail.com>
2023-01-19 20:37:55 +0000

    Made data exportable to text file

--- Overclock.py
+++ Overclock.py
@@ -17,7 +17,7 @@ def start():
 def Menu():
     os.system('cls')
     print(graphics.Menu)
-    print(" 1. View current configuration \n 2. Edit configuration \n 3. exit program")
+    print(" 1. View current configuration \n 2. Edit configuration \n 3. export configuration \n 4. exit program")
     choice = input(" user: ")
     if choice == "1":
         os.system('cls')
@@ -28,6 +28,8 @@ def Menu():
         os.system('cls')
         editor()
     elif choice == "3":
+        export()
+    elif choice == "4":
         existstage1()
     else: 
         Menu()
@@ -38,7 +40,7 @@ def editor():
     print(graphics.Warning)
     print(" You are edting data!")
     print("Don't use units")
-    CPUPowerLimit = str(input("CPU Power Limit (W): "))
+    CPUPowerLimit = float(input("CPU Power Limit (W): "))
     CPUClock = float(input("CPU Clock (Ghz): "))
     Gprocessor = input("GPU: ")
     GPUpowerLimit = float(input("GPU power limit (W): "))
@@ -47,7 +49,8 @@ def editor():
     BusClock = float(input("Bus Clock (Mhz): "))
     RyzenADJI = input("Ryzen ADJI preset (if you have one, if not leave blank): ")
     USBdevices = int(input("Amount of static USB devices: " ))
-    TotalUSBpower = float(USBdevices * 4.5)
+    TotalUSBpower = USBdevices * 4.5
+    TotalPower = (TotalUSBpower + GPUpowerLimit + CPUPowerLimit * 1.32)
     data = {
     "PCname": my_system.node,
     "System": f"{my_system.system} {my_system.version}",
@@ -61,7 +64,8 @@ def editor():
     "GPUMemoryclock": GPUMemoryClock,
     "RyzenADJI": RyzenADJI,
     "USBdevices": USBdevices,
-    "TotalUSBPower": TotalUSBpower
+    "TotalUSBPower": TotalUSBpower,
+    "TotalPower": TotalPower
     }
     with open("Configuationtable.json", "w") as file:
         file.write(json.dumps(data))
@@ -81,9 +85,33 @@ def configdisplay():
         print(" GPU Power Limit: ", data["GPUpowerLimit"], "W")
         print(" GPU Clock: ", data["GPUClock"], "Mhz")
         print("\n Ryzen ADJI, ", data["RyzenADJI"])
-        print("\n USB devices", data["USBdevices"])
-        print(" Total usb power", data["TotalUSBPower"])
+        print("\n Static USB devices:", data["USBdevices"])
+        print(" Total USB power: ", data["TotalUSBPower"])
+        print("\n Totalpower: ", data["TotalPower"])
         print(graphics.Rever)
+
+def export():
+    with open("Configuationtable.json", "r") as file:
+        data = json.loads(file.read())
+        with open("config_data.txt", "w") as file:
+            file.write("="*40 + " last Saved Configuation/System Information " + "="*40 + "\n")
+            file.write(" System: " + data["System"] + "\n")
+            file.write(" PC Name: " + data["PCname"] + "\n")
+            file.write("\n Processor: " + data["Processor"] + "\n")
+            file.write(" Processor Power Limit: " + data["CPUPowerLimit"] + "W" + "\n")
+            file.write(" Processor clock: " + str(data["CPUClock"]) + "Ghz" + "\n")
+            file.write(" Bus clock: " + str(data["BusClock"]) + "Mhz" + "\n")
+            file.write("\n GPU: " + data["Gprocessor"] + "\n")
+            file.write(" GPU Power Limit: " + str(data["GPUpowerLimit"]) + "W" + "\n")
+            file.write(" GPU Clock: " + str(data["GPUClock"]) + "Mhz" + "\n")
+            file.write(" GPU Memory clock: " + str(data["GPUMemoryclock"]) + "Mhz" + "\n")
+            file.write("\n Ryzen ADJI: " + data["RyzenADJI"] + "\n")
+            file.write("\n Static USB devices: " + str(data["USBdevices"]) + "\n")
+            file.write(" Total USB power: " + str(data["TotalUSBPower"]) + "W" + "\n")
+            file.write(" Total Power: " + str(data["TotalPower"]) + "W" + "\n")
+    print(" Data exported to config")
+    input(" Press enter to continue")
+    Menu()
 
 def existstage1():
     os.system('cls')
