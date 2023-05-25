a4b2e0a8b37e493a1a88a8b1054e5f7f3ff7928d
mrmarcus007 <mrmarcus00700909@gmail.com>
2023-05-25 14:17:45 +0100

    Update graphics.py

--- graphics.py
+++ graphics.py
@@ -3,7 +3,7 @@ import pyfiglet
 def ascii_art(text):
     return pyfiglet.figlet_format(text)
 
-Rever = " \n Overclock Manager, Version: 2.0.1"
+Rever = " \n Overclock Manager, Version: 3.5.1
 Overclock = ascii_art("Overclock Manager")
 Menu = ascii_art("Menu")
 Warning = ascii_art("Warning")
